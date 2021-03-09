6db49491c3f710e20b3646d5d96aad489c3fc557
Richard Morey <richarddmorey@gmail.com>
2021-03-09 15:59:10 +0000

    Update main.66b857e3aceb2ca30539.js

--- dist/main.66b857e3aceb2ca30539.js
+++ dist/main.66b857e3aceb2ca30539.js
@@ -137,7 +137,7 @@
 /******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
 /******/
 /******/ 	// __webpack_public_path__
-/******/ 	__webpack_require__.p = "/dist/";
+/******/ 	__webpack_require__.p = "/studentProjectAllocation/dist/";
 /******/
 /******/ 	var jsonpArray = window["webpackJsonp"] = window["webpackJsonp"] || [];
 /******/ 	var oldJsonpFunction = jsonpArray.push.bind(jsonpArray);
